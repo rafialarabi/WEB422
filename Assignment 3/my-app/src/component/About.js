b0865fae133986b56960998ca2212deff5798c6a
rafialarabi <84462357+rafialarabi@users.noreply.github.com>
2021-08-24 03:25:05 -0400

    Add files via upload

---
+++ Assignment 3/my-app/src/component/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { ListGroupItem } from "react-bootstrap";
+
+function About() {
+  return (
+    <ListGroupItem>
+      <h1>About</h1>
+      <br></br>
+      <p>
+        <strong>Student : </strong> Md Rafi Al Arabi Bhuiyan{" "}
+      </p>
+      <p>
+        <strong>ID # : </strong> 147307293{" "}
+      </p>
+      <p>
+        <strong>Purpose : </strong> WEB 422 - Assignment 3
+      </p>
+      <p>
+        <strong>Date : </strong> June 27, 2021
+      </p>
+    </ListGroupItem>
+  );
+}
+
+export default About;
